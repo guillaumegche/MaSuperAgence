e4aa57e51d6157d0622c3f1b898e39e40b6e36ec
guillaume gauche <gaucheguillaume@gmail.com>
2021-04-15 17:11:46 +0200

    feat frontend

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(PropertyRepository $repository): Response
+    {
+        $properties = $repository->findLatest();
+        return $this->render('home/home.html.twig', [
+            'properties' => $properties
+        ]);
+    }
+}
---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PropertyController extends AbstractController
+{
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/biens", name="property.index")
+     */
+    public function index(): Response
+    {
+        $properties = $this->repository->findAllVisible();
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties',
+            'properties' => $properties
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show(Property $property, string $slug)
+    {
+        /* On redirige vers la page de l'ID si il y a une erreur dans le slug est mauvais dans l'url */
+        if($property->getSlug() !== $slug)
+        {
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ]);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'current_menu' => 'properties',
+            'property' => $property
+        ]);
+    }
+}
